fdb7ac1d54323052388454d159fa7f7b829738ca
chrisdugne <chris.dugne@uralys.com>
2022-12-01 10:06:45 +0100

    started 2022 :art:

--- 2019/day-1.1.js
+++
@@ -1,4 +0,0 @@
-const spacecraftModules = require('./day-1-spacecraft-modules');
-const getFuelRequired = mass => Math.floor(mass / 3) - 2;
-const fuelRequired = spacecraftModules.map(getFuelRequired).reduce((acc, m) => acc + m, 0);
-console.log(fuelRequired);
